aed9459f10939c88beb8559219071476d07562fc
张锡铭 <2234587454@qq.com>
2017-09-20 17:04:18 +0800

    使用Lua脚本保证setnx与expire的原子性

--- src/main/java/com/ddtc/singleredisdistributedlock/lock/DistributeLock.java
+++ src/main/java/com/ddtc/singleredisdistributedlock/lock/DistributeLock.java
@@ -17,6 +17,15 @@ public class DistributeLock {
     private long lockTime = 0L; //获取到锁的时间
     private boolean lock = false; //锁状态
 
+
+    private static String script =
+            "if redis.call('setnx',KEYS[1],KEYS[2]) == 1 then\n"
+            +"redis.call('expire',KEYS[1],KEYS[3]);\n"
+            +"return 1;\n"
+            +"else\n"
+            +"return 0;\n"
+            +"end\n";
+
     DistributeLock(Jedis jedis, String key) {
         this.redisClient = jedis;
         this.key = key;
@@ -36,19 +45,25 @@ public class DistributeLock {
 
             while (System.currentTimeMillis() - startTime < timeout) {
                 //锁不存在的话，设置锁并设置锁过期时间，即加锁，并把value 定义为过期的时间点
-                if (this.redisClient.setnx(this.key, String.valueOf(System.currentTimeMillis() + expire * 1000)) == 1) {
-                    this.lockTime = System.currentTimeMillis();
-                    this.redisClient.expire(key, expire);//设置锁过期时间是为了在没有释放
-                    //锁的情况下锁过期后消失，不会造成永久阻塞
-                    this.lock = true;
-                    System.out.println(key + "加锁成功" + Thread.currentThread().getName());
-                    return this.lock;
-                } else {
-                    //如果Redis在超时后没有自动删除，则删除。。用于解决setnx 与 expire 不是原子操作的问题
-                    if (Long.valueOf(redisClient.get(key)) - System.currentTimeMillis() < 0) {
-                        redisClient.del(key);
-                    }
+                //this.redisClient.setnx(this.key, String.valueOf(System.currentTimeMillis() + expire * 1000)) == 1
+                //this.redisClient.expire(key, expire);//设置锁过期时间是为了在没有释放
+
+                //使用Lua脚本保证setnx与expire的原子性
+                Object object = redisClient.eval(script,3,key,"a",String.valueOf(expire));
+                if ((long)object==1) {
+                        this.lockTime = System.currentTimeMillis();
+                        //锁的情况下锁过期后消失，不会造成永久阻塞
+                        this.lock = true;
+                        System.out.println(key + "加锁成功" + Thread.currentThread().getName());
+                        return this.lock;
+
                 }
+//                else {
+//                    //如果Redis在超时后没有自动删除，则删除。。用于解决setnx 与 expire 不是原子操作的问题
+//                    if (Long.valueOf(redisClient.get(key)) - System.currentTimeMillis() < 0) {
+//                        redisClient.del(key);
+//                    }
+//                }
                 System.out.println("出现锁等待" + Thread.currentThread().getName());
                 //短暂休眠，避免可能的活锁
                 Thread.sleep(500);
